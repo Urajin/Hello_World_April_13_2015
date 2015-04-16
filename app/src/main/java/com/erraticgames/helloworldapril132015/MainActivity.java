ff5b229a0b85f0ea23b9d2d84922cbce5c4f5de2
Urajin <barry_n@cogeco.ca>
2015-04-16 13:22:14 -0400

    comments

--- app/src/main/java/com/erraticgames/helloworldapril132015/MainActivity.java
+++ app/src/main/java/com/erraticgames/helloworldapril132015/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+/*comment*/
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       int num = 10;
     }
 
 
